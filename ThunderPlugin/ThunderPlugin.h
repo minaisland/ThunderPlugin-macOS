dc2839b40f12c004ab4cd0630df73760261acd6b
阿东 <konaod@gmail.com>
2018-11-09 23:35:53 +0800

    Initial Commit

---
+++ ThunderPlugin/ThunderPlugin.h
@@ -0,0 +1,19 @@
+//
+//  ThunderPlugin.h
+//  ThunderPlugin
+//
+//  Created by Stanley on 2018/11/9.
+//  Copyright © 2018年 Stanley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ThunderPlugin.
+FOUNDATION_EXPORT double ThunderPluginVersionNumber;
+
+//! Project version string for ThunderPlugin.
+FOUNDATION_EXPORT const unsigned char ThunderPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThunderPlugin/PublicHeader.h>
+
+
